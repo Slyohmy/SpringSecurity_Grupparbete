80162e4263dab2903fd49fe9f1248e23e2fb55bd
Ramtin Pourshakour <ramtin_pourshakour@yahoo.com>
2022-05-13 11:21:16 +0200

    RabbitMQ initial

--- src/main/java/com/example/springboot_grupparbete/RabbitMQ/EventConsumer.java
+++
@@ -1,1 +0,0 @@
-package com.example.springboot_grupparbete.RabbitMQ;import org.slf4j.Logger;import org.slf4j.LoggerFactory;public class EventConsumer {  private Logger logger = LoggerFactory.getLogger(EventConsumer.class);  @RabbitListener(queues="orderServiceQueue")  public void receive(String message) {    logger.info("Received message '{}'", message);  }}
